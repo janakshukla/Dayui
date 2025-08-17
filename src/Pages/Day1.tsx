f3cc7218eeb29c48fc4f10a026911cdf9fb6cd76
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-17 20:31:48 +0530

    add animation to Day1 page and improve layout structure

--- src/Pages/Day1.tsx
+++ src/Pages/Day1.tsx
@@ -1,16 +1,19 @@
 const Day1 = () => {
-  return <div className="h-screen w-screen bg-Day1-primary flex justify-center p items-center " >
-
-    <div className=" h-4/5 w-5/6   " >
-    <h1 className="font-semibold text-5xl text-shadow-lg tracking-tighter font-geist text-center" >Pricing plans</h1>
-    <p className= " font-geist font-medium  text-shadow-lg -tracking-normal mt-4 text-center">Choose the right plan for your needs.</p>
-    <div className="h- w-full bg-red-400" >  
-    
-
+  return (
+    <div className="h-screen w-screen bg-Day1-primary flex justify-center p items-center ">
+      <div className=" h-4/5 w-5/6  animation  ">
+        <div className="h-1/5">
+          <h1 className="font-semibold text-5xl text-shadow-lg tracking-tighter font-geist text-center">
+            Pricing plans
+          </h1>
+          <p className=" font-geist font-medium  text-shadow-lg -tracking-normal mt-4 text-center">
+            Choose the right plan for your needs.
+          </p>
+        </div>
+        <div className="w-full h-4/5"></div>
+      </div>
     </div>
-    </div>
-    
-  </div>;
+  );
 };
 
 export default Day1;
