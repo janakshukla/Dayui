215fc6b03c5652d2f108e102e7d31312e18ae5cc
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-18 17:13:47 +0530

    add: create Button component and integrate it into Navbar for improved modularity

--- src/components/day2/Navbar.tsx
+++ src/components/day2/Navbar.tsx
@@ -1,5 +1,6 @@
 import Logo from "../../assets/day2/Logo.png";
-import shop from "../../assets/day2/shop.png";
+import Button from "./Button";
+
 const Navbar = () => {
   return (
     <div className=" m-auto w-3/5 bg-white mt-8 drop-shadow-sm drop-shadow-[#FF675C]/20 rounded-2xl flex justify-between items-center py-2 px-2  ">
@@ -8,10 +9,7 @@ const Navbar = () => {
         <li className="cursor-pointer hover:scale-110" > Home</li>
         <li className="cursor-pointer hover:scale-110" > How it works</li>
         <li className="cursor-pointer hover:scale-110"  >Resources</li>
-        <button className="py-2 px-2 bg-[#FF7A68] cursor-pointer hover:scale-110 flex gap-0.5 rounded-lg  text-white drop-shadow-sm drop-shadow-[#FF5E49]/50 shadow-[inset_0_-3.57px_11.78px_0_#EB4630,inset_0_0.89px_0.89px_0px_#FFFFFF] ">
-          <img src={shop} alt="shop" />
-          Install Customizer{" "}
-        </button>
+       <Button/>
       </ul>
     </div>
   );
