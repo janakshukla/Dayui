accc89f10648025702a34bd1540041756d8d39cd
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-18 17:54:21 +0530

    style: enhance Button and Navbar components with responsive design adjustments

--- src/components/day2/Button.tsx
+++ src/components/day2/Button.tsx
@@ -2,8 +2,8 @@ import shop from "../../assets/day2/shop.png";
 
 const Button = () => {
   return (
-    <button className="py-1 px-2 text-sm font-medium font-geist bg-[#FF7A68] cursor-pointer hover:scale-110 flex gap-0.5 rounded-lg  text-white drop-shadow-sm drop-shadow-[#FF5E49]/50 shadow-[inset_0_-3.57px_11.78px_0_#EB4630,inset_0_0.89px_0.89px_0px_#FFFFFF] ">
-    <img src={shop} alt="shop" />
+    <button className="py-1 sm:px-2 text-xs sm:text-sm font-medium font-geist bg-[#FF7A68] cursor-pointer hover:scale-110 flex sm:gap-0.5 rounded-lg  text-white drop-shadow-sm drop-shadow-[#FF5E49]/50 shadow-[inset_0_-3.57px_11.78px_0_#EB4630,inset_0_0.89px_0.89px_0px_#FFFFFF] ">
+    <img className="scale-50 sm:scale-100" src={shop} alt="shop" />
     Install Customizer{" "}
   </button> 
   )
--- src/components/day2/Navbar.tsx
+++ src/components/day2/Navbar.tsx
@@ -3,9 +3,9 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className=" m-auto w-3/5 bg-white mt-8 drop-shadow-sm drop-shadow-[#FF675C]/20 rounded-2xl flex justify-between items-center py-2 px-2  ">
-      <img src={Logo} alt="logo" />
-      <ul className="flex gap-4 text-sm tracking-tight font-medium font-geist items-center ">
+    <div className=" m-auto sm:w-3/5  bg-white mt-8 drop-shadow-sm drop-shadow-[#FF675C]/20 rounded-2xl flex justify-between items-center py-2 px-2  ">
+      <img className="scale-75 sm:scale-100" src={Logo} alt="logo" />
+      <ul className="flex sm:gap-4 gap-2 text-xs sm:text-sm tracking-tight font-medium font-geist items-center ">
         <li className="cursor-pointer hover:scale-110" > Home</li>
         <li className="cursor-pointer hover:scale-110" > How it works</li>
         <li className="cursor-pointer hover:scale-110"  >Resources</li>
