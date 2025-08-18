9622a8da8c86a5917cd7f2b43d14d37ded55adb7
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-18 11:06:46 +0530

    fix: correct grammar and punctuation in Home page description

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -18,9 +18,7 @@ const Home = () => {
           Welcome to Dayui.
         </h1>
         <p className=" font-geist font-medium  text-shadow-lg -tracking-normal mt-4 text-center">
-          Here i am recreating every thing what @heysatya_ will design in his 30
-          days challenge.this home ui is not final i will be creating its design
-          on day 31.
+        Here I am, recreating everything that @heysatya_ will design in his 30-day challenge. This home UI is not final; I will be creating its design on Day 31.
         </p>
       </div>
       <div>
