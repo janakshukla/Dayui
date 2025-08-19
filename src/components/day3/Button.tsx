9a57383f7ae89b6d57d615e9a38861ffe0890aeb
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-19 15:10:30 +0530

    add: enhance Button and ContentCard components with props for dynamic rendering

--- src/components/day3/Button.tsx
+++ src/components/day3/Button.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
 
-const Button = () => {
+
+const Button = ({
+    value,
+    color
+}: {
+    value: string;
+    color: string;
+}) => {
   return (
-    <div>Button</div>
+    <button className={`${color} text-white px-12 py-2 rounded-xl shadow-[inset_0px_1px_2px_0_#FFFFFF]/17 drop-shadow-[inset_0px_12px_22px_0_#000000]/45`} >{value}</button>
   )
 }
 
--- src/components/day3/ContentCard.tsx
+++ src/components/day3/ContentCard.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Button from './Button'
 
 const ContentCard = () => {
   return (
-    <div>ContentCard</div>
+    <div>
+        <div></div>
+        <div className='flex w-full justify-between px-4' >
+            <Button value='Hire Me' color='bg-[#FFFFFF]/20' />
+            <Button value='Copy Email' color='bg-[#FFFFFF]/10' />
+             </div>
+    </div>
   )
 }
 
