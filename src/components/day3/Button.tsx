accb1a49199c75f695f7e07022af8e792bacad53
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-19 15:20:10 +0530

    add: update Day 3 page layout and enhance Button and ContentCard components with new features

--- src/components/day3/Button.tsx
+++ src/components/day3/Button.tsx
@@ -8,7 +8,9 @@ const Button = ({
     color: string;
 }) => {
   return (
-    <button className={`${color} text-white px-12 py-2 rounded-xl shadow-[inset_0px_1px_2px_0_#FFFFFF]/17 drop-shadow-[inset_0px_12px_22px_0_#000000]/45`} >{value}</button>
+    <button onClick={()=>{
+        window.location.href = "https://github.com/janakshukla"
+      }} className={`${color} text-white px-20 py-2 rounded-xl shadow-[inset_0px_1px_2px_0_#FFFFFF]/17 drop-shadow-[inset_0px_12px_22px_0_#000000]/45`} >{value}</button>
   )
 }
 
--- src/components/day3/ContentCard.tsx
+++ src/components/day3/ContentCard.tsx
@@ -1,10 +1,18 @@
 import Button from './Button'
+import Avatar from "../../assets/day3/Avatar.png"
 
 const ContentCard = () => {
   return (
     <div>
-        <div></div>
-        <div className='flex w-full justify-between px-4' >
+        <div className='flex w-full gap-4 mt-4 px-4' >
+            <img src={Avatar} alt="Avatar" />
+            <div>
+                <h1 className='text-white text-2xl font-inter ' >Janak Shukla</h1>
+                <p className='text-white/50  font-inter '>Full Stack Developer</p>
+            </div>
+            
+             </div>
+        <div className='flex mt-4 w-full justify-between px-4' >
             <Button value='Hire Me' color='bg-[#FFFFFF]/20' />
             <Button value='Copy Email' color='bg-[#FFFFFF]/10' />
              </div>
