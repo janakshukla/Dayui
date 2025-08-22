df02324318d686c74d5a5992d3729bb50e25c1d6
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-22 21:13:03 +0530

    fix: simplify SideBarFooter layout for improved responsiveness and consistency

--- src/components/day4/SideBarFooter.tsx
+++ src/components/day4/SideBarFooter.tsx
@@ -4,9 +4,7 @@ import { ChevronRight } from "lucide-react";
 const SideBarFooter = ({ isopen }: { isopen: boolean }) => {
   return (
     <div
-      className={`h-1/10 flex justify-center items-center scale-75 xl:scale-100 hover:bg-gray-300  font-inter max-w-full ${
-        isopen ? "  " : ""
-      } `}
+      className={`h-1/10 flex sm:justify-center items-center scale-75 xl:scale-100 hover:bg-gray-300  font-inter max-w-full `}
     >
       <img className="scale-50" src={Avatar} alt="Avatar" />
       <div className={`${isopen ? "" : "hidden"}`}>
