5993c82397e6eb177531197d482ea57d224e491e
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-23 17:32:01 +0530

    refactor: remove Footer component and replace with Companies image; enhance HeroPart with new content and styling

--- src/Pages/Day5.tsx
+++ src/Pages/Day5.tsx
@@ -1,15 +1,15 @@
-import Footer from "../components/day5/Footer";
+import Companies from "../assets/day5/companies.svg"
 import HeroPart from "../components/day5/HeroPart";
 import Nav from "../components/day5/Nav";
 
 const Day5 = () => {
   return (
     <div className="bg-gray-950 h-screen min-w-full p-4 ">
-      <div className="h-11/12 bg-[#2B2B2B]/54 outline outline-white/21 backdrop-blur-3xl drop-shadow-md drop-shadow-[#000000]/25 rounded-4xl ">
+      <div className="h-11/12 bg-[#2B2B2B]/54 overflow-hidden outline outline-white/21 backdrop-blur-3xl drop-shadow-md drop-shadow-[#000000]/25 rounded-4xl ">
         <Nav />
         <HeroPart />
       </div>
-      <Footer />
+     <img className="w-1/2 m-auto mt-2" src={Companies} alt="footer" />
     </div>
   );
 };
--- src/components/day5/HeroPart.tsx
+++ src/components/day5/HeroPart.tsx
@@ -1,7 +1,24 @@
+import Hero from "../../assets/day5/Hero.png"
+
 const HeroPart = () => {
   return (
-    <div>HeroPart</div>
-  )
-}
+    <div className="flex flex-col items-center gap-4 justify-center mt-6 h-[90%] ">
+      <h1 className="font-inter text-center tracking-tighter leading-[3rem]  text-5xl font-bold bg-linear-to-r from-white/92 to-[#999999]/92 text-transparent bg-clip-text">
+        The Smarter Way to <br />  Manage Emails
+      </h1>
+      <p className="text-center text-[#D9D9D9] text-[10px]" >
+        Auto-segmentation, smart filtering, AI auto-complete, and built-in
+        CRM. <br /> Stop wasting time on repetitive tasks.
+      </p>
 
-export default HeroPart
+      <button className="font-medium mt-4  text-xs hover:bg-[#7CFF36]/80 bg-[#7CFF36] px-4 py-3 rounded-4xl  drop-shadow-xl drop-shadow-[#7FDE4B]/51 ">
+        {" "}
+        Request Access{" "}
+      </button>
+      <p className="text-center -mt-2 text-white/70 text-[8px]" >Get 3 month free now</p>
+      <img className=" scale-95 -mt-8 " src={Hero} alt="hero" />
+    </div>
+  );
+};
+
+export default HeroPart;
