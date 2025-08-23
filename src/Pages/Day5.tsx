536778cd335e8f9818f1b8bdc91572858ae23ceb
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-23 17:37:48 +0530

    add: enhance Day5 page background with gradient effect and improve layout structure

--- src/Pages/Day5.tsx
+++ src/Pages/Day5.tsx
@@ -5,7 +5,10 @@ import Nav from "../components/day5/Nav";
 const Day5 = () => {
   return (
     <div className="bg-gray-950 h-screen min-w-full p-4 ">
-      <div className="h-11/12 bg-[#2B2B2B]/54 overflow-hidden outline outline-white/21 backdrop-blur-3xl drop-shadow-md drop-shadow-[#000000]/25 rounded-4xl ">
+       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[600px] h-[700px] 
+              bg-gradient-to-b from-white via-white/50 to-transparent 
+              rounded-full blur-3xl opacity-70 pointer-events-none"></div>
+      <div className=" relative h-11/12 bg-[#2B2B2B]/54 overflow-hidden outline outline-white/21 backdrop-blur-3xl drop-shadow-md drop-shadow-[#000000]/25 rounded-4xl ">
         <Nav />
         <HeroPart />
       </div>
