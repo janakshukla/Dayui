8d6fc663199719fe757e8db2f2bcf57ac2e4a70e
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-24 15:24:57 +0530

    add: enhance HeroSection with new layout and styling; update Button component appearance

--- src/components/Home/Button.tsx
+++ src/components/Home/Button.tsx
@@ -4,7 +4,7 @@ const Button = () => {
   return (
     <button className="bg-green-300 text-xs p-2 rounded-4xl flex items-center justify-center gap-2 text-gray-950 shadow-inner hover:bg-green-400 transition-colors duration-300 shadow-black/40 drop-shadow-sm drop-shadow-black/20  ">
         Get Started for free
-        <ArrowRight className="" size={16} />
+        <ArrowRight className="bg-gray-900 rounded-2xl text-white/80 " size={18} />
     
     </button>
   )
--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import Companies from "../../assets/day5/companies.svg";
+import Button from "./Button";
+import {BookmarkCheck } from "lucide-react"
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div className="flex flex-col items-center gap-6 justify-center text-center p-4 ">
+      <BookmarkCheck className="p-3 size-12 border border-white backdrop-blur-2xl bg-white/10 text-white rounded-xl rotate-12 " />
+      <h1 className="text-6xl text-white/80 tracking-tighter  font-serif " >One Place For <br /> Every Design you Make. </h1>
+      <p className=" text-white/70 tracking-tight font-inter text-sm " >This Library is more of a result of @_heysataya design challenge <br /> where i developed everything pixel perfect from his designs,if you like any thing feel free to use. </p>
+      <Button />
+      <img className="sm:w-1/2 m-auto mt-2" src={Companies} alt="footer" />
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
