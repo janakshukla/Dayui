40ae513d196d07507cc9737338462155ff4bbfc7
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-08-24 15:03:14 +0530

    add: implement Button component and enhance NavBar with new styling; update Home page layout with background image

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-import Day1 from "../assets/image.png"
-import Day2 from "../assets/day2/day2.png"
+import Day1 from "../assets/image.png";
+import Day2 from "../assets/day2/day2.png";
 import Day3 from "../assets/day3/day3.png";
 import Day4 from "../assets/day4/day4.png";
 import Day5 from "../assets/day5/day5.png";
 import NavBar from "../components/Home/NavBar";
 import HeroSection from "../components/Home/HeroSection";
+import bgimage from "../assets/Home/hero.png";
 const Home = () => {
   const days = [
     {
@@ -42,19 +43,22 @@ const Home = () => {
       description: "Sprrrint email client",
       image: Day5,
       link: "/day5",
-    }
-    
+    },
   ];
 
   return (
-    <div className="min-h-screen w-full bg-orange-50 p-4 ">
-      <div className="h-screen">
-        <NavBar/>
-        <HeroSection/>
-       </div>
+    <div className="min-h-screen w-full bg-orange-50 ">
+      <div
+        className="bg-cover container relative object-fill min-h-screen min-w-full"
+        style={{ backgroundImage: `url(${bgimage})` }}
+      >
+        <div className="absolute inset-0 bg-gray-950/50">
+          <NavBar />
+          <HeroSection />
+        </div>
+      </div>
       <div>
-       
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+        <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 mt-4">
           {days.map((day) => (
             <Link to={day.link} key={day.day}>
               <div className="bg-white hover:bg-gradient-to-r  from-[#C145FF]/20 to-[#1FDDF2]/20 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
@@ -64,7 +68,7 @@ const Home = () => {
               </div>
             </Link>
           ))}
-          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Home/NavBar.tsx
+++ src/components/Home/NavBar.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Button from "./Button"
+
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className='w-full  p-4 flex items-center justify-between ' >
+        <h1 className="text-2xl tracking-tighter font-bold font-geist text-white/80 " ><span className=" font-serif text-3xl text-yellow-500" >D</span>ayUi</h1>
+        <Button/>
+    </nav>
   )
 }
 
