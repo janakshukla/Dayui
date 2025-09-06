befc42a3ce436478e9aac9e642e25f1e216dd968
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-09-06 16:08:49 +0530

    add: enhance Day8 components with improved styling and new features in Hero, Footer, and Navbar

--- src/components/day8/Hero.tsx
+++ src/components/day8/Hero.tsx
@@ -1,22 +1,37 @@
 import { Card, CardFooter, CardHeader } from "./Card";
 import BGhero from "../../assets/day8/hero.png";
+import { ArrowRightIcon,Circle } from "lucide-react";
 
 const Hero = () => {
   return (
-    <div className="h-full">
+    <div className="h-full  flex justify-around items-center-safe   ">
       <Card
-        className="w-1/2 h-1/2 mx-auto mt-10 flex flex-col items-center justify-between text-center gap-6 text-white bg-cover bg-center bg-no-repeat "
+        className="w-1/2 h-2/3  flex p-2 flex-col items-center justify-between text-center gap-6 text-white bg-cover bg-center bg-no-repeat "
         style={{
           backgroundImage: `url(${BGhero})`,
         }}
       >
-        <CardHeader></CardHeader>
-        <CardFooter className="">
-          <button className="border border-white py-2 text-white w-2/3 rounded-full hover:bg-blue-700 transition-colors duration-300">
+        <CardHeader
+        className="flex justify-end-safe "
+        >
+          <div className="flex gap-2 bg-black px-5   py-3 -mt-2.5 -mr-2.5 rounded-bl-3xl">
+          <Circle size={10} fill="#FFFFFF" />
+          <Circle size={10} />
+          </div>
+         
+        </CardHeader>
+        <CardFooter>
+          <button className="border flex items-center justify-between  border-white p-2 text-white w-2/3 rounded-full hover:backdrop-blur-3xl transition-all duration-300">
             Book Demo call{" "}
+            <ArrowRightIcon
+              className="p-1 rounded-full bg-white "
+              size={26}
+              color="#000000"
+            />
           </button>
         </CardFooter>
       </Card>
+      <h1 className="text-white/70 uppercase  text-6xl font-thin leading-20 tracking-tight "  >exiciting <br /> ideas await <br /> you,each <br /> coooler than <br /> the last!</h1>
     </div>
   );
 };
--- src/components/day8/Navbar.tsx
+++ src/components/day8/Navbar.tsx
@@ -2,7 +2,7 @@ import {SquareUserRound,ArrowRightCircleIcon} from 'lucide-react'
 
 const Navbar = () => {
   return (
-    <nav className="w-11/12 mx-auto text-[#b2b2b2] flex items-center border  justify-between p-2 mt-2 rounded-4xl">
+    <nav className="w-11/12  mx-auto text-[#b2b2b2] flex items-center border  justify-between p-2 mt-2 rounded-4xl">
         <div className='flex items-center justify-around w-1/3' >
             <SquareUserRound size={32} fill='#2B4491' />
             <p className='uppercase font-medium text-sm ' >services</p>
@@ -11,7 +11,7 @@ const Navbar = () => {
 
 
         </div>
-        <button className='flex items-center gap-1 border border-white px-2 py-0.5 rounded-2xl text-xs '>SIGN UP <ArrowRightCircleIcon fill='#FFFFFF' color='#000000' /> </button>
+        <button className='flex items-center gap-1 border hover:bg-white/30 transition-all duration-300 border-white px-2 py-0.5 rounded-2xl text-xs '>SIGN UP <ArrowRightCircleIcon fill='#FFFFFF' color='#000000' /> </button>
 
     </nav>
   )
