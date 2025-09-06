ad08aae42340df8fb067291b3459901b24583774
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-09-06 15:28:39 +0530

    add: implement Card component with Header and Footer for Day8 page

---
+++ src/components/day8/Card.tsx
@@ -0,0 +1,64 @@
+import cn from "classnames";
+
+const Card = ({
+  children,
+  className,
+  style
+}: {
+  children?: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+}) => {
+  return (
+    <div
+      className={cn(
+        "bg-white/10 border border-white/20 rounded-3xl  transition-transform duration-300 ease-in-out",
+        className
+
+      )}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CardHeader = ({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "w-full flex  justify-between",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CardFooter = ({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+    className={cn(
+      "w-full flex  justify-between",
+      className
+    )}
+  >
+    {children}
+  </div>
+  );
+};
+
+export { Card, CardHeader, CardFooter };
--- src/components/day8/Hero.tsx
+++ src/components/day8/Hero.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Card, CardFooter, CardHeader } from "./Card";
+import BGhero from "../../assets/day8/hero.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="h-full">
+      <Card
+        className="w-1/2 h-1/2 mx-auto mt-10 flex flex-col items-center justify-between text-center gap-6 text-white bg-cover bg-center bg-no-repeat "
+        style={{
+          backgroundImage: `url(${BGhero})`,
+        }}
+      >
+        <CardHeader></CardHeader>
+        <CardFooter className="">
+          <button className="border border-white py-2 text-white w-2/3 rounded-full hover:bg-blue-700 transition-colors duration-300">
+            Book Demo call{" "}
+          </button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
