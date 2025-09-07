4a9e92bf78fb444d517b36f64758876ad4f0c455
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-09-07 19:59:00 +0530

    add: improve Navbar component styling with responsive padding and adjusted gap

--- src/components/day8/Navbar.tsx
+++ src/components/day8/Navbar.tsx
@@ -3,10 +3,10 @@ import {SquareUserRound,ArrowRightCircleIcon} from 'lucide-react'
 const Navbar = () => {
   return (
    <div>
-     <nav className="w-11/12  mx-auto text-[#b2b2b2] flex items-center border  justify-between p-2  rounded-4xl">
-        <div className='flex items-center justify-around gap-2 w-1/3' >
+     <nav className="w-11/12  mx-auto text-[#b2b2b2] flex items-center border  justify-between sm:p-2 p-1 rounded-4xl">
+        <div className='flex items-center justify-around gap-1 w-1/3' >
             <SquareUserRound size={32} fill='#2B4491' />
-            <p className='uppercase text-nowrap font-medium text-xs ' >about <br /> us</p>
+            <p className='uppercase text-nowrap font-medium text-xs ' >about  us</p>
             <p className='uppercase text-nowrap font-medium text-xs ' >services</p>
             <p className='uppercase text-nowrap font-medium text-xs ' >cases</p>
 
